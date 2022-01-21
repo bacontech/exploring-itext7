22d86722a4d124cf05883007162ddd5907744edc
Alex Bacon <fosizzle@gmail.com>
2022-01-21 11:08:58 -0600

    spring start

---
+++ src/main/java/com/bacontech/exploreitext7/ExploreItext7Application.java
@@ -0,0 +1,13 @@
+package com.bacontech.exploreitext7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreItext7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreItext7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bacontech/exploreitext7/ExploreItext7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bacontech.exploreitext7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreItext7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
